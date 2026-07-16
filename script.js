f7110ecb3e1d1bcfe6f6f038a346655bb9449241
motojpnesford <moto7692@pb.ctt.ne.jp>
2026-07-16 20:01:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("NMS REFINER Ver0.1");
+const text=document.getElementById("bootText");
+const bar=document.getElementById("bar");
+
+const steps=[
+["INITIALIZING...",25],
+["CONNECTING TO DATABASE...",55],
+["VERIFYING RECIPES...",80],
+["WELCOME TRAVELLER",100]
+];
+
+let i=0;
+
+function nextStep(){
+
+text.innerHTML=steps[i][0];
+
+bar.style.width=steps[i][1]+"%";
+
+i++;
+
+if(i<steps.length){
+
+setTimeout(nextStep,700);
+
+}else{
+
+setTimeout(()=>{
+
+document.getElementById("boot").style.display="none";
+
+document.getElementById("main").style.display="block";
+
+document.body.style.overflow="auto";
+
+},900);
+
+}
+
+}
+
+nextStep();
