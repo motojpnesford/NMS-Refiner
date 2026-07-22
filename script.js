6a51387c200e0d3a1b9f228394fed4e175263fbb
motojpnesford <moto7692@pb.ctt.ne.jp>
2026-07-22 22:04:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -286,4 +286,13 @@ ${getItemName(c.result.item)}
      
     `;
 
+    const blocks = itemBody.querySelectorAll("h3, ul, p");
+
+blocks.forEach((block, index) => {
+
+    block.classList.add("reveal");
+
+    block.style.animationDelay = `${index * 0.12}s`;
+
+});
 });
